0f91c37be9d28e33a677fed79e17943cfed3f7f4
OlhaDorofiychuk <olha.d@yahoo.com>
2022-11-07 10:35:53 +0000

    2 extensions to finish

--- src/Button.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-const [NumberTwo, setInputTwo] = useState("0");
-function Button() {
-  for (let i = 0; i < 10; i++) {
-    return (setInputTwo = i);
-  }
-  return <button onClick={() => setInputTwo("i")}>i</button>;
-}
--- src/calculator.js
+++ src/calculator.js
@@ -1,63 +1,135 @@
-// import { click } from "@testing-library/user-event/dist/click";
 import { useState } from "react";
 function Calculator() {
   const [NumberOne, setInputOne] = useState("0");
   const [NumberTwo, setInputTwo] = useState("0");
-  let [Operation, setOperation] = useState("+");
-  const [Result, setResult] = useState("");
-  function Add() {
-    return NumberOne + NumberTwo;
-  }
-  setOperation = Add();
+  const [Operation, setOperation] = useState("+");
+  let [Result, setResult] = useState("0");
+  const [StoreResult, setStoreResult] = useState("");
+  const multiNumber = (numberSetInput, number, newNum) => {
+    if (number === "0") {
+      // && number[1])!=='.')
+      // && !number.includes(".")
+      numberSetInput(newNum);
+    } else {
+      numberSetInput(number + newNum);
+    }
+  };
+
+  const CalculateResult = () => {
+    const allOperators = {
+      "+": parseInt(NumberOne) + parseInt(NumberTwo),
+      "-": parseInt(NumberOne) - parseInt(NumberTwo),
+      "*": parseInt(NumberOne) * parseInt(NumberTwo),
+      "÷": parseInt(NumberOne) / parseInt(NumberTwo),
+    };
+
+    setResult(allOperators[Operation]);
+  };
+
+  const Recall = () => {};
+  //   setResult = () => {
+  //     if (Result.length > 6) {
+  //       Number.parseFloat(Result).toFixed(5);
+  //     }
+  //   };
   return (
     <div className="calculator">
       <div className="panel">
-        <p>NumberOne {NumberOne}</p>
+        <p>{NumberOne}</p>
         <div className="numbers">
-          <button onClick={(target) => setInputOne("1")}>1</button>
-          <button onClick={(event) => setInputOne("2")}>2</button>
-          <button onClick={(event) => setInputOne("3")}>3</button>
-          <button onClick={(event) => setInputOne("4")}>4</button>
-          <button onClick={(event) => setInputOne("5")}>5</button>
-          <button onClick={(event) => setInputOne("6")}>6</button>
-          <button onClick={(event) => setInputOne("7")}>7</button>
-          <button onClick={(event) => setInputOne("8")}>8</button>
-          <button onClick={(event) => setInputOne("9")}>9</button>
-          <button onClick={(event) => setInputOne("0")}>0</button>
-          <button>Clear</button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "1")}>
+            1
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "2")}>
+            2
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "3")}>
+            3
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "4")}>
+            4
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "5")}>
+            5
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "6")}>
+            6
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "7")}>
+            7
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "8")}>
+            8
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "9")}>
+            9
+          </button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, "0")}>
+            0
+          </button>
+          <button onClick={() => setInputOne("0")}> Clear</button>
+          <button onClick={() => multiNumber(setInputOne, NumberOne, ".")}>
+            .
+          </button>
+          <button onClick={() => StoreResult}>Recall</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>Operation{Operation}</p>
+        <p>{Operation}</p>
         <div className="numbers">
-          <button onClick={setOperation}>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          <button onClick={() => setOperation("+")}>+</button>
+          <button onClick={() => setOperation("-")}>-</button>
+          <button onClick={() => setOperation("*")}>*</button>
+          <button onClick={() => setOperation("÷")}>÷</button>
         </div>
       </div>
 
       <div className="panel">
-        <p>InputTwo{NumberTwo}</p>
+        <p>{NumberTwo}</p>
         <div className="numbers">
-          <button onClick={() => setInputTwo("1")}>1</button>
-          <button onClick={() => setInputTwo("2")}>2</button>
-          <button onClick={() => setInputTwo("3")}>3</button>
-          <button onClick={() => setInputTwo("4")}>4</button>
-          <button onClick={() => setInputTwo("5")}>5</button>
-          <button onClick={() => setInputTwo("6")}>6</button>
-          <button onClick={() => setInputTwo("7")}>7</button>
-          <button onClick={() => setInputTwo("8")}>8</button>
-          <button onClick={() => setInputTwo("9")}>9</button>
-          <button onClick={() => setInputTwo("0")}>0</button>
-          <button>Clear</button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "1")}>
+            1
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "2")}>
+            2
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "3")}>
+            3
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "4")}>
+            4
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "5")}>
+            5
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "6")}>
+            6
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "7")}>
+            7
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "8")}>
+            8
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "9")}>
+            9
+          </button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, "0")}>
+            0
+          </button>
+          <button onClick={() => setInputTwo("0")}>Clear</button>
+          <button onClick={() => multiNumber(setInputTwo, NumberTwo, ".")}>
+            .
+          </button>
+          <button onClick={() => StoreResult}>Recall</button>
         </div>
       </div>
       <div className="panel answer">
-        <p>Result {Result}</p>
+        <p>{Result}</p>
         <div>
-          <button onClick={() => setResult(Add)}>=</button>
+          <button onClick={() => CalculateResult()}>=</button>
+          <button onClick={() => setStoreResult}>Store Result</button>
         </div>
       </div>
     </div>
